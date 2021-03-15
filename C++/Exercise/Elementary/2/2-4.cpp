ab19e2f82a67f8b3dc22349c55bad937e40badc3
Danielyc0304 <79126857+Danielyc0304@users.noreply.github.com>
2021-03-15 22:02:16 +0800

    C++

--- C++/Exercise/Elementary/2/2-4.cpp
+++ C++/Exercise/Elementary/2/2-4.cpp
@@ -25,9 +25,38 @@ void quicksort(int *num, int left, int right){
         i=left;
         j=right+1;
 
+        while(1){
+            while((i<right) && (num[++i]<num[left]));
+            while((j>0) && (num[--j]>num[left]));
 
+            if(i>=j)
+                break;
+            swap(&num[i], &num[j]);
+        }
+        swap(&num[left], &num[j]);
+
+        cout<<"L="<<left<<" j="<<j<<" R="<<right<<endl;
+        print(num, Size);
+
+        quicksort(num, left, j-1);
+        quicksort(num, j+1, right);
     }
 }
 int main(){
+    int num[Size];
+    int i;
+
+    srand(time(NULL));
+
+    for(i=0; i<Size; ++i)
+        num[i]=rand()%1000+1;
+    cout<<"排序前\n";
+    print(num, Size);
+
+    quicksort(num, 0, Size-1);
+
+    cout<<"排序後\n";
+    print(num, Size);
+
     return 0;
 }
