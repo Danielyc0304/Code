484c4afa4fd4861ddc055f3470e585a00c0d0722
Daniel Lin <danielyc.mail0304@gmail.com>
2021-04-04 18:02:00 +0800

    C++

--- C++/1.cpp
+++ C++/1.cpp
@@ -40,7 +40,6 @@ void mergesort(int *num, int left, int right){
     }
 }
 int main(){
-<<<<<<< HEAD
     int num[10000], N;
     int i;
 
@@ -52,8 +51,5 @@ int main(){
 
     cout<<ans<<endl;
 
-=======
-    
->>>>>>> dcbd0637ff40531cb1df271bcda65ccc057f5b4b
     return 0;
 }
--- C++/ZeroJudge/a/a746.cpp
+++ C++/ZeroJudge/a/a746.cpp
@@ -4,15 +4,9 @@ using namespace std;
 
 int main(){
     char a[503][503];//a圖案
-<<<<<<< HEAD
-    int n, m, x, y;//n圖案邊長; m座標總數; x, y座標
-    int tmpx=0, tmpy=0;//tmpx, tmpy座標暫存值
-    int i, j;//i, j旗標
-=======
     int n, m, x, y;//n圖案邊長; m座標總數; x, y座標
     int tmpx=0, tmpy=0;//tmpx, tmpy座標暫存值
     int i, j, k;//i, j, k旗標
->>>>>>> dcbd0637ff40531cb1df271bcda65ccc057f5b4b
     
     memset(a, ' ', sizeof(a));//初始化a為空格
 
