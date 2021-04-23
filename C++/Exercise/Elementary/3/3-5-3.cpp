e0466cc2ebce15107e019afc5a15813972283708
Daniel Lin <danielyc.mail0304@gmail.com>
2021-04-24 00:21:12 +0800

    C++

---
+++ C++/Exercise/Elementary/3/3-5-3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int num_conversion(string a){
+    if(a=="FO" || a=="GO" || a=="SO") return 0;
+    if(a=="1B") return 1;
+    if(a=="2B") return 2;
+    if(a=="3B") return 3;
+    if(a=="HR") return 4;
+}
+int main(){//棒球遊戲(10510第4題)
+    
+    return 0;
+}
