37cbf36a755f5369cff5fe01cbe2d99e06769c8c
Daniel Lin <danielyc.mail0304@gmail.com>
2021-05-15 10:14:12 +0800

    Python

--- Python/Runoob/017.py
+++ Python/Runoob/017.py
@@ -1,10 +1,21 @@
 import string
 
 a=input('請輸入一個字符串:\n')
-c1=0
-c2=0
-c3=0
-c4=0
+ans1=0
+ans2=0
+ans3=0
+ans4=0
 
 for i in a:
-    if 
+    if i.isalpha(): ans1+=1
+    elif i.isspace(): ans2+=1
+    elif i.isdigit(): ans3+=1
+    else: ans4+=1
+print('char = %d,space = %d,digit = %d,others = %d' %(ans1, ans2, ans3, ans4))
+'''
+Input:
+請輸入一個字符串:
+123runoobc  kdf235*(dfl
+Output:
+char = 13,space = 2,digit = 6,others = 2
+'''
